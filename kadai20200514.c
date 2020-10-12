fa98e2586581b00c1cd54e0e71fe0915a4e3ceb5
Hiroto Tamura <56391366+pipanolis@users.noreply.github.com>
2020-10-12 04:02:15 -0500

    Update kadai20200514.c
    make things useful

--- kadai20200514.c
+++ kadai20200514.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void){
-    int f[] = {0,0,0,0,0,0,0,0,0,0};
-    f[0] = 0;
-    f[1] = 1;
+#define NUMOFENTRIES 10
+#define FIRST 0
+#define SECOND 1
 
-    int i;
-    for(i=0; i<=7; i+=1){
+int main(void){
+    
+    int f[NUMOFENTRIES];
+    f[0] = FIRST;
+    f[1] = SECOND;
+    
+    for(int i = 0; i < (NUMOFENTEIS - 2); i += 1){
         f[i+2] = f[i+1] + f[i];
     }
-
-    printf("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d", f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9]);
+    
+    for(int i = 0; i < NUMOFENTEIS; i += 1){
+       printf("%d\n", f[i]);
+    }
 
     return 0;
 }
